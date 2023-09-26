15512e5761e59a8b82e523e523e1961725145698
Juan <j.v.delcruz70120@gmail.com>
2023-09-26 14:20:59 -0700

    lets go

---
+++ Develop/index.js
@@ -0,0 +1,84 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
+const fs = require('fs');
+
+const path = require('path');
+
+const generateMarkdown = require('./assets/utils/generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+
+    {
+        type: 'input',
+        name: 'github',
+        message: "What's your GitHub account username?"
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message: "What's your email?"
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: "What's the name of your project?"
+    },
+
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please provide a brief description of your project?"
+    },
+
+    {
+        type: 'list',
+        name: 'license',
+        message: "Choose the appropriate license for your application",
+        choices: ["Apache 2.0", "MIT", "GPL v3", "No License"]
+    },
+
+    {
+        type: 'input',
+        name: 'dependencies',
+        message: "What dependencies need to be install prior to running?",
+    },
+
+    {
+        type: 'input',
+        name: 'test',
+        message: "What command do you enter to run the test?",
+    },
+
+    {
+        type: 'input',
+        name: 'contributors',
+        message: "Who are the main contributors of this repository?"
+    },
+    {
+
+        type: 'input',
+        name: 'usage',
+        message: 'Give a brief description of the usage of this application.'
+    },
+    
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerAnswers) => {
+        console.log("Generating your request...");
+        writeToFile("./assets/utils/sampleREADME.md", generateMarkdown({ ...inquirerAnswers }));
+    })
+}
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,94 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license === "") {
+    return "";
+  } else {
+    return `${renderLicenseLink(license)}`
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+  }
+  if (license === 'GPL v3') {
+    return `![License: GPL](https://img.shields.io/badge/License-GPL-blue.svg)`
+  }
+  if (license === 'Apache 2.0') {
+    return `![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
+  }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return renderLicenseBadge(license);
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  ${renderLicenseSection(data.license)}
+
+## Description 
+
+  ${data.description}
+
+  ## Table of Contents 
+
+  * [Installations](#installations)
+
+  * [Usage](#usage)
+  
+  * [Contributors](#contributors)
+
+  * [Test](#tests)
+
+  * [Questions](#questions)
+
+  * ${renderLicenseLink(data.license)}
+  
+  ## Installations
+
+  To install dependencies, run these commands:
+
+  \`\`\`
+  ${data.dependencies}
+  \`\`\`
+
+  ## Usage
+  
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributors
+
+  ${data.contributors}
+
+  ## Tests
+
+  To run tests, run these commands:
+
+  \`\`\`
+  ${data.test}
+  \`\`\`
+
+  ## Questions
+
+  For additional questions or would like to proide additional feedback, please contact me at my email below.
+
+  - GitHub: [${data.github}](https://github.com/${data.github}/)
+  - Email:  ${data.email}
+  `;
+}
+
+
+module.exports = generateMarkdown;
